f8311fc3e3cce45e6a8ec53aebdc0f9035a15949
yusuke13 <you326@gmail.com>
2017-08-23 13:21:45 +0900

    Add files via upload

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,17 @@
 import webpack from 'webpack';
 import path from 'path';
-import ExtracTextPlugin from 'extract-text-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
 
 module.exports = {
   entry : {
-    "sideMenu-controller" : './src/js/sideMenu-controller.js'
+    "sideMenu-controller" : path.resolve(__dirname, 'src/js/sideMenu-controller.js'),
+    "css_common" : path.resolve(__dirname, 'src/js/css_common.js')
   },
   output : {
-    path : path.resolve(__dirname, './release/js'),
+    //path : __dirname + '/release/js/',
+    path : path.resolve(__dirname, 'release/js'),
+    publicPath : '/release/js/',
     filename : '[name].js'
   },
   module : {
@@ -21,7 +25,35 @@ module.exports = {
           }
         },
         exclude : /node_modules/,
+      },
+      {
+        test : /\.scss$/,
+        use : ExtractTextPlugin.extract(
+          {
+            fallback : 'style-loader',
+            use : [{
+              loader : 'css-loader'
+            },{
+              loader : 'sass-loader'
+            },{
+              loader : 'resolve-url-loader'
+            }]
+          }
+        )
+      },
+      {
+        test : /\.(jpe?g|png|gif)$/i,
+        use : {
+          loader : 'url-loader'
+        }
       }
     ]
-  }
+  },
+  resolve : {
+    extensions : ['.js', '.jsx', '.scss'],
+    modules : ['node_modules', __dirname]
+  },
+  plugins : [
+    new ExtractTextPlugin('/release/css/[name].css')
+  ]
 };
